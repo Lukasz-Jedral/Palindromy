c62be05ca8004f7e55678ad0e6e3e614ed27790b
Lukasz Jedral <geodezja.jedral@gmail.com>
2020-06-29 17:15:24 +0200

    Add is_palindromy() help description

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,4 +1,9 @@
 def is_palindrome(word):
+    """
+        Checks if given word is palindrome.
+        Accepts only strings as arguments.
+        Returns True of False.
+    """
     if type(word) == str:
         rev_word = word[::-1]
         if word.capitalize() == rev_word.capitalize():
